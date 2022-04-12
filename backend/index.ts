b7b899d511dd3548bf79650c7ba937ebfa906069
Joshua Santos <JoshCSan@gmail.com>
2022-04-11 23:22:51 -0700

    cleaned comments

--- backend/index.ts
+++ backend/index.ts
@@ -2,18 +2,9 @@ import express from 'express';
 import middleware from './server/util/middleware';
 import router from './server/router';
 
-// const main = () => {
 const app = express();
 
 middleware(app);
 router(app);
 
-// const port = import.meta.env.VITE_PORT || 8080;
-
-// app.listen(port, () => {
-//   console.log(`server listening on ${port}`);
-// });
-// };
-
-// export const viteNodeApp = app;
-export const viteApp = app
+export const viteApp = app;
