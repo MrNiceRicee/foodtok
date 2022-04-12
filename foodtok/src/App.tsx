fc5c43013f607dc9458c1df5fb1e2078e30c372d
Joshua Santos <JoshCSan@gmail.com>
2022-04-12 00:57:50 -0700

    removed esbuild override

--- foodtok/src/App.tsx
+++ foodtok/src/App.tsx
@@ -2,8 +2,8 @@ import ThemeButton from './darkmode';
 
 function App() {
   return (
-    <div className="bg-sky-300 dark:bg-sky-900">
-      <h1 className="text-3xl font-bold underline text-violet-300 dark:text-yellow-200">
+    <div className="bg-sky-300 dark:bg-sky-900 container !max-w-4xl mx-auto flex justify-center ">
+      <h1 className="text-3xl font-bold underline dark:text-violet-300 text-yellow-200 px-10">
         Hey!
       </h1>
       <ThemeButton />
