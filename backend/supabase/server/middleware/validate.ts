787b1cb7568abc954a36c3d34dc789283bfdf843
Joshua Santos <JoshCSan@gmail.com>
2022-05-07 11:08:42 -0700

    cleaning up backend

--- backend/supabase/server/middleware/validate.ts
+++ backend/supabase/server/middleware/validate.ts
@@ -2,21 +2,19 @@ import { NextFunction, Request, Response } from 'express';
 import supabase from '@util/supabase';
 
 const validateJWT = async (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers.authorization?.replace('Bearer ', '')
-  if (!token) {
-    res.status(401).send('unauthorized').end();
-    return;
-  }
+  const token = req.headers.authorization?.replace('Bearer ', '');
+  if (!token) return res.status(401).send('unauthorized').end();
+
   const { data, error } = await supabase.auth.api.getUser(token);
   if (error) {
-    console.log(error)
+    console.log(error);
     res.status(error.status).send(error.message).end();
     return;
   }
-  if (!data) {
-    res.status(404).send('could not find associated user').end();
-    return;
-  }
+
+  if (!data)
+    return res.status(404).send('could not find associated user').end();
+
   req['user'] = data;
   req.headers['foodtok-user'] = data.id;
   next();
