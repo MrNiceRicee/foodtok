0a8dd08fc760f54c118986657d995b294cac2aca
Joshua Santos <JoshCSan@gmail.com>
2022-05-09 20:38:29 -0700

    changed lengths on db

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE "Users"(
   _id UUID NOT NULL,
-  "name" VARCHAR(240) NOT NULL,
-  "displayName" VARCHAR(240),
+  "name" VARCHAR(26) NOT NULL,
+  "displayName" VARCHAR(26),
   "url" TEXT,
   "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
   "updatedAt" TIMESTAMP NOT NULL DEFAULT NOW(),
@@ -11,7 +11,7 @@ CREATE TABLE "Users"(
 CREATE TABLE "Recipes"(
   _id INT GENERATED ALWAYS AS IDENTITY,
   "name" VARCHAR(26) NOT NULL,
-  "description" VARCHAR(20),
+  "description" VARCHAR(240),
   "url" TEXT,
   "longUrl" TEXT,
   "UserId" UUID,
@@ -22,13 +22,13 @@ CREATE TABLE "Recipes"(
 );
 CREATE TABLE "Categories"(
   _id INT GENERATED ALWAYS AS IDENTITY,
-  "name" VARCHAR(240) NOT NULL,
+  "name" VARCHAR(16) NOT NULL,
   "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
   PRIMARY KEY (_id)
 );
 CREATE TABLE "Ingredients"(
   _id INT GENERATED ALWAYS AS IDENTITY,
-  "name" VARCHAR(240) NOT NULL,
+  "name" VARCHAR(16) NOT NULL,
   "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
   UNIQUE("name"),
   PRIMARY KEY (_id)
@@ -39,7 +39,7 @@ CREATE TABLE "Recipes_Categories"(
   "RecipeId" INT,
   "CategoryId" INT,
   "UserId" UUID,
-  "name" varchar(240),
+  "name" varchar(16),
   "description" varchar(240),
   CONSTRAINT "Recipes_Categories_RecipeId_fk" FOREIGN KEY ("RecipeId") REFERENCES "Recipes"(_id) ON UPDATE CASCADE ON DELETE CASCADE,
   CONSTRAINT "Recipes_Categories_CategoryId_fk" FOREIGN KEY ("CategoryId") REFERENCES "Categories"(_id) ON UPDATE CASCADE ON DELETE CASCADE,
@@ -53,7 +53,7 @@ CREATE TABLE "Recipes_Ingredients"(
   "UserId" UUID,
   "description" VARCHAR(240),
   "servingSize" DECIMAL,
-  "servingUnit" VARCHAR(240),
+  "servingUnit" VARCHAR(20),
   CONSTRAINT "Recipes_Ingredients_RecipeId_fk" FOREIGN KEY ("RecipeId") REFERENCES "Recipes"(_id) ON UPDATE CASCADE ON DELETE CASCADE,
   CONSTRAINT "Categories_UserId_fk" FOREIGN KEY ("UserId") REFERENCES "Users"(_id) ON UPDATE CASCADE ON DELETE CASCADE,
   "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
