6d00f902d3d317bab1565d5b59363b5283c50eb2
Joshua Santos <JoshCSan@gmail.com>
2022-06-04 16:02:27 -0700

    - update: added react-table - update: hover effect

--- foodtok/src/components/Table/Body.tsx
+++ foodtok/src/components/Table/Body.tsx
@@ -2,7 +2,7 @@ import { Cell } from './types';
 
 const Row = ({ row }: { row: Array<Cell> }) => {
   return (
-    <tr>
+    <tr className="dark:hover:bg-neutral-900 hover:bg-slate-100">
       {row.map((cell, idx) => (
         <td className="prose dark:text-slate-100" key={idx}>
           <span>{cell.display || cell.value}</span>
