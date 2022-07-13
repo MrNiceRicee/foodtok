4834175a96db240b190a4a2f428f8d773053b1ee
Joshua Santos <JoshCSan@gmail.com>
2022-07-13 00:24:57 -0700

    - update: attempt to make docker-slim run

--- backend/supabase/server/util/supabase.ts
+++ backend/supabase/server/util/supabase.ts
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL || process.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_KEY || process.env.VITE_SUPABASE_KEY,
-);
+const supabase = !process.env.PROBE
+  ? createClient(
+      import.meta.env.VITE_SUPABASE_URL || process.env.VITE_SUPABASE_URL,
+      import.meta.env.VITE_SUPABASE_KEY || process.env.VITE_SUPABASE_KEY
+    )
+  : createClient('', '');
 
 export default supabase;
